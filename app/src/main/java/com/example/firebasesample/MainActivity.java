0102bb123977dafb5f844a1b369d7d16ba8d7f0b
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-16 18:42:43 +0300

    added mediastore connection

--- app/src/main/java/com/example/firebasesample/MainActivity.java
+++ app/src/main/java/com/example/firebasesample/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.firebasesample;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
+    private Uri imagsUri;
+    private Bitmap Images;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.choice_image);
+        imageView=findViewById(R.id.choice_image);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==1){
+            if (permissions.equals(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                if (grantResults.length>0){
+                    Intent intent=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    startActivityForResult(intent,2);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==2){
+            if (data!=null){
+                if (resultCode==RESULT_OK){
+                    imagsUri=data.getData();
+                    try {
+                        Images=MediaStore.Images.Media.getBitmap(this.getContentResolver(),imagsUri);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
     }
 }
